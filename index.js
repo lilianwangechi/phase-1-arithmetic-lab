ca6d2947b04d360ed594082bb9c56bfd6ff41384
lilian <lilian.wangechi@student.moringaschool.com>
2022-08-09 04:08:30 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+//is an equation whose multiplied results will equal 62
+let num1= 31;
+let num2 = 2;
+let multiply = (num1 * num2)
+
+// generates a random integer greater than 0
+let random = (Math.floor(Math.random() * 10) + 1);
+
+//is an equation that calculates a remainder that is equal to 4
+let num3 = 34;
+let num4 = 6;
+let mod = (num3 % num4)
+
+//will return 20 as the highest number in the set
+let x = 5;
+let y = 6;
+let z = 20;
+let max = (Math.max(x,y,z))
